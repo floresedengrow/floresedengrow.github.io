004dfa5c38c88e1048505450655ef0df782f4c8f
manuelcalavera <manuelcalavera@protonmail.com>
2021-05-28 16:33:58 -0300

    Ahora si

--- mainjs.js
+++ mainjs.js
@@ -2,6 +2,6 @@ $(function () {
     $(".b-html").append($('<div>').load("https://static2.floresdeleden.com.ar/socialnetwork.html"));
     $("body").append($('<div>').load("https://static2.floresdeleden.com.ar/whatsapp.html"));
 
-    $(".ef-level-1 > li > a").first().text("TODOS");
-    $(".eshop-filter-select > .wnd-filter-select > option").first().text("TODOS");
+    $(".ef-level-1 li a").first().text("TODOS");
+    $(".eshop-filter-select .wnd-filter-select option:selected").first().text("TODOS");
 });
