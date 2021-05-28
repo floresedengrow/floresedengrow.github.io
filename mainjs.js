0311cc5ae5a7b26e71aa74e2ef519d0a194f7a07
manuelcalavera <manuelcalavera@protonmail.com>
2021-05-28 12:06:18 -0300

    Changes

--- mainjs.js
+++ mainjs.js
@@ -1,4 +1,4 @@
 $(function () {
-    $(".b-html").load("https://static2.floresdeleden.com.ar/socialnetwork.html");
-    $("footer").load("https://static2.floresdeleden.com.ar/whatsapp.html");
+    $(".b-html").append("https://static2.floresdeleden.com.ar/socialnetwork.html");
+    $("footer").append($('<div>').load("https://static2.floresdeleden.com.ar/whatsapp.html"));
 });
