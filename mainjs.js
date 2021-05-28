445b348460279ca8e78142a32bbb1e8476104465
manuelcalavera <manuelcalavera@protonmail.com>
2021-05-28 12:08:50 -0300

    s

--- mainjs.js
+++ mainjs.js
@@ -1,4 +1,4 @@
 $(function () {
-    $(".b-html").append("https://static2.floresdeleden.com.ar/socialnetwork.html");
+    $(".b-html").append($('<div>').load("https://static2.floresdeleden.com.ar/socialnetwork.html"));
     $("footer").append($('<div>').load("https://static2.floresdeleden.com.ar/whatsapp.html"));
 });
