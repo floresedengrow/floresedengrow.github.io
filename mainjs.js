ce46a9ea7b7e1d73104e2694db58751552eb5f97
manuelcalavera <manuelcalavera@protonmail.com>
2021-05-28 16:30:34 -0300

    Hola

--- mainjs.js
+++ mainjs.js
@@ -3,4 +3,5 @@ $(function () {
     $("body").append($('<div>').load("https://static2.floresdeleden.com.ar/whatsapp.html"));
 
     $(".ef-level-1 > li > a").first().text("TODOS");
+    $(".eshop-filter-select > .wnd-filter-select > option").first().text("TODOS");
 });
