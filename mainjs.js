3f4fcc556a9824d42005c15bd990c77c861542c7
manuelcalavera <manuelcalavera@protonmail.com>
2021-06-06 16:15:08 -0300

    ,00 Delete

--- mainjs.js
+++ mainjs.js
@@ -4,8 +4,19 @@ $(function () {
 
     $(".ef-level-1 li a").first().text("TODOS");
     $(".eshop-filter-select .wnd-filter-select option:selected").first().text("TODOS");
-});
 
-$( document ).ajaxComplete(function() {
-    $(".ef-level-1 li a").first().text("TODOS");
-});
+}); 
+
+(function() {
+    var origOpen = XMLHttpRequest.prototype.open;
+    XMLHttpRequest.prototype.open = function() {
+        this.addEventListener('load', function() {
+            $(".ef-level-1 li a").first().text("TODOS");
+            $(".eshop-filter-select .wnd-filter-select option:selected").first().text("TODOS");
+            $(".product-price-content").text(function(i, v) {
+              return v.replace(/\,00/, "");
+            });
+        });
+        origOpen.apply(this, arguments);
+    };
+})();
